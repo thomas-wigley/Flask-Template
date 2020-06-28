cbbdd45004af0fc86ed0f055111a7ee43b8dbaf8
Thomas Wigley <66223217+thomas-wigley@users.noreply.github.com>
2020-06-28 17:55:01 +0100

    added caching

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_assets import Environment, Bundle
+from flask_caching import Cache
+
 
 
 app = Flask(__name__)
@@ -20,10 +22,17 @@ assets.register({
     )
 })
 
+cache = Cache(app, config={
+    'CACHE_TYPE': 'simple',
+    'CACHE_DEFAULT_TIMEOUT': 24*60*60
+})
+
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 
 if __name__ == '__main__':
